e20ca0ae9f2b5c2b0ed7129018453067fa7fadd2
Mashiro-jz <a.wiacek@zlotoryja.xyz>
2026-05-11 10:16:51 +0200

    Update filePath to relative path for transactions

--- src/main/java/com/finance_manager/infrastructure/repository/JsonTransactionRepository.java
+++ src/main/java/com/finance_manager/infrastructure/repository/JsonTransactionRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class JsonTransactionRepository implements TransactionRepository {
-    String filePath ="C:\\Intelij_IDEA\\Developer_Projects\\Finance_Manager\\src\\transaction.json";
+    String filePath ="src\\transaction.json";
     private final ObjectMapper mapper;
 
     public JsonTransactionRepository() {
